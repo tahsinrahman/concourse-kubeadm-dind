d1925b68f8ab84a300df608cebfab46b0faacc53
tahsinrahman <tahsynx@gmail.com>
2018-04-21 17:07:38 +0600

    fix test script

--- test.sh
+++ test.sh
@@ -2,6 +2,8 @@
 
 set -x
 
+apt-get update &> /dev/null
+apt-get install -y wget vim curl docker.io &> /dev/null
 curl -LO https://raw.githubusercontent.com/meAmidos/dcind/master/docker-lib.sh
 chmod +x docker-lib.sh
 
@@ -9,9 +11,6 @@ source docker-lib.sh
 start_docker
 
 docker run hello-world
-
-apt-get update &> /dev/null
-apk-get install -y curl wget &> /dev/null
 
 wget https://cdn.rawgit.com/Mirantis/kubeadm-dind-cluster/master/fixed/dind-cluster-v1.8.sh
 chmod +x dind-cluster-v1.8.sh
